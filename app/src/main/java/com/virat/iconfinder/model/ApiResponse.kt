44d1ad90a8294ea1ab7a60908a9cd3b2f5ae14a3
virat <vs9358@srmist.edu.in>
2022-05-19 13:34:23 +0530

    fresh  code

---
+++ app/src/main/java/com/virat/iconfinder/model/ApiResponse.kt
@@ -0,0 +1,5 @@
+package com.virat.iconfinder.model
+
+class ApiResponse (
+    val icons: List<Icon>
+)
---
+++ app/src/main/java/com/virat/iconfinder/model/Price.kt
@@ -0,0 +1,6 @@
+package com.virat.iconfinder.model
+
+class Price (
+    val currency: String,
+    val price: String
+)
